b00bf594302d013dca9fce02f1e41a9de46cd646
Himanshu1446 <hsingla@iitk.ac.in>
2018-12-05 22:14:14 +0530

    327B

---
+++ 327B.cpp
@@ -0,0 +1,20 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include<string.h>
+using namespace std;
+int main()
+{ 
+    long long n;
+    cin >>n;
+    for(int i=0;i<n;i++)
+    {
+        cout<< n+i<<" ";
+        
+    }
+
+    
+ 
+}
