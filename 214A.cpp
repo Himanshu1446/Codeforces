de877ff634f9f7951f16f57bd8d06c38260de3bb
Himanshu1446 <hsingla@iitk.ac.in>
2018-12-10 01:19:25 +0530

    system of equations

---
+++ 214A.cpp
@@ -0,0 +1,22 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include<string.h>
+using namespace std;
+int main()
+{
+   int n,m,count=0;
+    cin>>n>>m;
+    for(int i=0;i<=max(n,m);i++)
+    {
+        for(int j=0;j<=max(n,m);j++)
+        {
+            if((i*i + j == n)&&(i + j*j ==m))
+                ++count;
+        }
+    }
+    cout<<count;
+    
+}
