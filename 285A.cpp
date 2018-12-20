c87d101a1123a7ed9117477928c47817cd675ff7
Himanshu1446 <hsingla@iitk.ac.in>
2018-12-21 01:23:17 +0530

    slightly decreasing permutation

---
+++ 285A.cpp
@@ -0,0 +1,21 @@
+#include <cmath>
+#include <iomanip>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include<string.h>
+using namespace std;
+int main()
+{
+    int n,k;
+    cin >>n>>k;
+    
+    for(int i=k+1;i>=1;i--)
+        cout<<i<<" ";
+    for(int i=k+2;i<=n;i++)
+        cout<<i<<" ";
+
+    
+}
+    
